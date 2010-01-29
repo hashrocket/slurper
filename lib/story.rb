2fc09441c120395da67ece23a1116a039565aa25
Wes Gibbs <dev+wgibbs@hashrocket.com>
2010-01-29 06:14:55 -0500

    Move story_defaults.yml to ~/.slurper.yml

--- lib/story.rb
+++ lib/story.rb
@@ -2,7 +2,7 @@ require 'active_resource'
 
 class Story < ActiveResource::Base
 
-  @@defaults = YAML.load_file('story_defaults.yml')
+  @@defaults = YAML.load_file(File.expand_path(File.join("~", ".slurper.yml")))
   self.site = "http://www.pivotaltracker.com/services/v3/projects/#{@@defaults['project_id']}"
   headers['X-TrackerToken'] = @@defaults.delete("token")
   attr_accessor :story_lines
