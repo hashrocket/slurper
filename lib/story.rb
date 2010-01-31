d62eb8339474e228e30278885a3a50e32fb65739
Wes Gibbs <wes@hashrocket.com>
2010-01-31 16:47:37 -0500

    Look for a slurper_config.yml in working directory

--- lib/story.rb
+++ lib/story.rb
@@ -2,7 +2,7 @@ require 'active_resource'
 
 class Story < ActiveResource::Base
 
-  @@defaults = YAML.load_file(File.expand_path(File.join("~", ".slurper.yml")))
+  @@defaults = YAML.load_file('slurper_config.yml')
   self.site = "http://www.pivotaltracker.com/services/v3/projects/#{@@defaults['project_id']}"
   headers['X-TrackerToken'] = @@defaults.delete("token")
   attr_accessor :story_lines
